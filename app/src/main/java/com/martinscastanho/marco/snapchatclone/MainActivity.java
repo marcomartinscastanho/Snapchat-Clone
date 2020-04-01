8190f936bfc08e0310222dd0045f5790185bb945
Marco Martins Castanho <marco.castanho@gmail.com>
2020-04-01 16:36:38 +0200

    initial commit

---
+++ app/src/main/java/com/martinscastanho/marco/snapchatclone/MainActivity.java
@@ -0,0 +1,42 @@
+package com.martinscastanho.marco.snapchatclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText usernameEditText;
+    EditText passwordEditText;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        // Initialize Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    public void loginButtonClick(View view){
+
+    }
+}
